0a8ec564add888a2defa3d92a582281fb9fb9d7d
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-04-03 20:47:01 +0600

    Add carriage

---
+++ app/controllers/carriages_controller.rb
@@ -0,0 +1,61 @@
+class CarriagesController < ApplicationController
+  # before_action :set_train, only: [ :new, :create ]
+  before_action :set_carriage, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @carriages = Carriage.all
+  end
+
+  def show
+  end
+
+  def new
+    @carriage = Carriage.new
+  end
+
+  def edit
+  end
+
+  def create
+    @carriage = Carriage.new(carriage_params)
+
+    if @carriage.save
+      redirect_to @carriage
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @carriage.update(carriage_params)
+      redirect_to @carriage
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @carriage.destroy
+    redirect_to carriages_path
+  end
+
+  private
+
+  # def set_train
+  #   @train = Train.find(params[:train_id])
+  # end
+
+  def set_carriage
+    @carriage = Carriage.find(params[:id])
+  end
+
+  def carriage_params
+    params.require(:carriage).permit(:carriage_type,
+                                                           :number,
+                                                           :top_seat,
+                                                           :bottom_seat,
+                                                           :side_top_seat,
+                                                           :side_bottom_seat,
+                                                           :train_id)
+  end
+end
