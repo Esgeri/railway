907b3fbc87d7e8a842b6a2019ab3ee550fc883ae
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-04-05 00:03:39 +0600

    Add scope for carriage model and its inheritance

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -2,4 +2,8 @@ class Carriage < ApplicationRecord
   belongs_to :train
 
   validates :number, :top_seats, :bottom_seats, presence: true
+
+  scope :economy, -> { where(carriage_type: 'EconomyCarriage') }
+  scope :coupe, -> { where(carriage_type: 'CoupeCarriage') }
+  scope :ordered, -> { order(:number) }
 end
