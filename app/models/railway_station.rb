e6b91c2a92af9332d618fef4398968b032d2fcef
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-04-08 01:46:07 +0600

    Add arrival and departure times for each station in the route

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -9,11 +9,26 @@ class RailwayStation < ApplicationRecord
 
   def update_position(route, position)
     station_route = station_route(route)
-    station_route.update(position: position) if station_route
+    station_route.update(position: position) if station_route(route)
   end
 
   def position_in(route)
     station_route(route).try(:position)
+  end
+
+  def change_time(route, arrival_time, departure_time)
+    station_route = station_route(route)
+    station_route.update(arrival_time: arrival_time, departure_time: departure_time) if station_route(route)
+  end
+
+  def change_arrival_time_in(route)
+    arrive_time = station_route(route).try(:arrival_time)
+    arrive_time.strftime('%T') unless arrive_time.nil?
+  end
+
+  def change_departure_time_in(route)
+    departure_time = station_route(route).try(:departure_time)
+    departure_time.strftime('%T') unless departure_time.nil?
   end
 
   protected
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405191846) do
+ActiveRecord::Schema.define(version: 20170407181342) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -33,9 +33,11 @@ ActiveRecord::Schema.define(version: 20170405191846) do
   end
 
   create_table "railway_stations_routes", force: :cascade do |t|
-    t.integer "railway_station_id"
-    t.integer "route_id"
-    t.integer "position",           default: 0
+    t.integer  "railway_station_id"
+    t.integer  "route_id"
+    t.integer  "position",           default: 0
+    t.datetime "arrival_time"
+    t.datetime "departure_time"
   end
 
   create_table "routes", force: :cascade do |t|
