a444b7b5de4fa0dbfcf64b506bfd335bda74862b
Mirbek Askerov <mirbek.askerov@gmail.com>
2017-04-08 03:21:28 +0600

    Refacting carriage model, controller, views and train's show page

--- app/controllers/carriages_controller.rb
+++ app/controllers/carriages_controller.rb
@@ -1,28 +1,49 @@
 class CarriagesController < ApplicationController
   before_action :set_train, only: [ :new, :create ]
+  before_action :set_carriage, only: [ :show, :edit, :update, :destroy ]
 
   def show
-    @carriage = Carriage.find(params[:id])
   end
 
   def new
     @carriage = Carriage.new
   end
 
+  def edit
+  end
+
   def create
     @carriage = @train.carriages.new(carriage_params)
 
     if @carriage.save
-      redirect_to @train
+      redirect_to @carriage.train, notice: "Carriage was successfully created."
     else
       render :new
     end
+  end
+
+  def update
+    if @carriage.update(carriage_params)
+      redirect_to @carriage.train, notice: "Carriage was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    train = @carriage.train
+    @carriage.destroy
+    redirect_to train, notice: "Carriage was successfully destroyed."
   end
 
   private
 
   def set_train
     @train = Train.find(params[:train_id])
+  end
+
+  def set_carriage
+    @carriage = Carriage.find(params[:id])
   end
 
   def carriage_params
